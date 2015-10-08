9d1a3930ea4ef26780888f09d6c58f0f5724e55f
hubertjb <hubert.jbanville@gmail.com>
2015-10-07 21:41:13 -0400

    changing init to pyOSC

--- museio_version/muse_server_pyosc.py
+++ museio_version/muse_server_pyosc.py
@@ -1,10 +1,10 @@
-from liblo import *
+from OSC import *
 
 import sys 
 import time
 
 
-class MuseServer(ServerThread):
+class MuseServer(ThreadingOSCServer):
     """
     Server to receive Muse data through OSC, based on pyOSC.
     pyOSC has two versions (http://stackoverflow.com/questions/22135511/a-plethora-of-python-osc-modules-which-one-to-use)
@@ -17,42 +17,52 @@ class MuseServer(ServerThread):
     Inspired by http://developer.choosemuse.com/research-tools-example/grabbing-data-from-museio-a-few-simple-examples-of-muse-osc-servers
     """
     #listen for messages on port 5000
-    def __init__(self, port=5000, printMsg=False):
-        ServerThread.__init__(self, port)
+    def __init__(self, host='127.0.0.1', port=5060, printMsg=False):
         
+        ThreadingOSCServer.__init__(self, server_address=(host,port))  
+        self.serve_forever()
         self.configFound = False
+        
+        self.addMsgHandler("/muse/eeg", self.format_eeg_handler)
 
-    #receive accelrometer data
-    @make_method('/muse/acc', 'fff')
-    def acc_callback(self, path, args):
-        acc_x, acc_y, acc_z = args
-        if printMsg:
-            print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
-
-    #receive EEG data
-    @make_method('/muse/eeg', 'ffff')
-    def eeg_callback(self, path, args):
-        l_ear, l_forehead, r_forehead, r_ear = args
-        if printMsg:
-            print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+    
+    #receive accelerometer data
+#    @make_method('/muse/acc', 'fff')
+#    def acc_callback(self, path, args):
+#        acc_x, acc_y, acc_z = args
+#        if printMsg:
+#            print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
             
-    @make_method('/muse/config', 's')
-    def config_callback(self, path, args):
-        if not self.configFound:
-            self.config = args
-            self.configFound = True
-            # TODO: TRANSFORM JSON TO DICTIONARY!!!
-
-    #handle unexpected messages
-    @make_method(None, None)
-    def fallback(self, path, args, types, src):
-        if printMsg:
-            print "Unknown message \
-            \n\t Source: '%s' \
-            \n\t Address: '%s' \
-            \n\t Types: '%s ' \
-            \n\t Payload: '%s'" \
-            % (src.url, path, types, args)   
+    def format_eeg_handler(addr, tags, packet, source):
+        if addr=='/muse/eeg':
+            l_ear, l_forehead, r_forehead, r_ear = packet
+            if printMsg:
+                print "%s %f %f %f %f" % (addr, l_ear, l_forehead, r_forehead, r_ear)
+#
+#    #receive EEG data
+#    @make_method('/muse/eeg', 'ffff')
+#    def eeg_callback(self, path, args):
+#        l_ear, l_forehead, r_forehead, r_ear = args
+#        if printMsg:
+#            print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+#            
+#    @make_method('/muse/config', 's')
+#    def config_callback(self, path, args):
+#        if not self.configFound:
+#            self.config = args
+#            self.configFound = True
+#            # TODO: TRANSFORM JSON TO DICTIONARY!!!
+#
+#    #handle unexpected messages
+#    @make_method(None, None)
+#    def fallback(self, path, args, types, src):
+#        if printMsg:
+#            print "Unknown message \
+#            \n\t Source: '%s' \
+#            \n\t Address: '%s' \
+#            \n\t Types: '%s ' \
+#            \n\t Payload: '%s'" \
+#            % (src.url, path, types, args)   
       
     def getparams(self):
         if self.configFound:
@@ -68,11 +78,18 @@ if __name__ == "__main__":
     try:
         server = MuseServer(printMsg=True)
         
-    except ServerError, err:
+    except 'allo':
+        
+        server.close()
+        
         print str(err)
         sys.exit()
     
-    server.start()
-    
-    while 1:
-        time.sleep(1)
+#    try:
+#        server = MuseServer(printMsg=True)
+#        
+#    
+#    server.start()
+#    
+#    while 1:
+#        time.sleep(1)
