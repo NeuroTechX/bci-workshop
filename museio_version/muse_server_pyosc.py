913bdc62f9142c42640bfe53e04aeea82c478db9
hubertjb <hubert.jbanville@gmail.com>
2015-11-05 23:28:28 -0500

    adding handlers for acc, config and other packets

--- museio_version/muse_server_pyosc.py
+++ museio_version/muse_server_pyosc.py
@@ -8,7 +8,7 @@ class MuseServer(ThreadingOSCServer):
     """
     Server to receive Muse data through OSC, based on pyOSC.
     pyOSC has two versions (http://stackoverflow.com/questions/22135511/a-plethora-of-python-osc-modules-which-one-to-use)
-    One works with Python 2.7, and the one on GitHub works with Python 3.0
+    One works with Python 2.7, and the one on GitHub works with Python 3.0.
     
     First, make sure the Muse SDK is installed, and run muse-io in a console by 
     calling:
@@ -17,56 +17,55 @@ class MuseServer(ThreadingOSCServer):
     Inspired by http://developer.choosemuse.com/research-tools-example/grabbing-data-from-museio-a-few-simple-examples-of-muse-osc-servers
     """
     #listen for messages on port 5000
-    def __init__(self, host='127.0.0.1', port=5060, printMsg=False):
+    def __init__(self, host='127.0.0.1', port=9999, printMsg=False):
         
-        ThreadingOSCServer.__init__(self, server_address=(host,port))  
-        self.serve_forever()
+        ThreadingOSCServer.__init__(self, server_address=(host,port))
+        self.printMsg = printMsg
         self.configFound = False
         
-        self.addMsgHandler("/muse/eeg", self.format_eeg_handler)
+    def startServer(self):
+        self.addMsgHandler('/muse/eeg', self.format_eeg_handler)
+        self.addMsgHandler('/muse/acc', self.format_acc_handler)
+        self.addMsgHandler('muse/config', self.format_config_handler)
 
-    
-    #receive accelerometer data
-#    @make_method('/muse/acc', 'fff')
-#    def acc_callback(self, path, args):
-#        acc_x, acc_y, acc_z = args
-#        if printMsg:
-#            print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
+        self.addMsgHandler('muse/eeg/quantization', self.format_other_handler)
+        self.addMsgHandler('muse/version', self.format_other_handler)
+        self.addMsgHandler('muse/batt', self.format_other_handler)
+        self.addMsgHandler('muse/drlref', self.format_other_handler)
+        self.addMsgHandler('muse/elements', self.format_other_handler)     
+        
+        self.serve_forever()
             
-    def format_eeg_handler(addr, tags, packet, source):
+    def format_eeg_handler(self, addr, tags, packet, source):
         if addr=='/muse/eeg':
             l_ear, l_forehead, r_forehead, r_ear = packet
-            if printMsg:
-                print "%s %f %f %f %f" % (addr, l_ear, l_forehead, r_forehead, r_ear)
-#
-#    #receive EEG data
-#    @make_method('/muse/eeg', 'ffff')
-#    def eeg_callback(self, path, args):
-#        l_ear, l_forehead, r_forehead, r_ear = args
-#        if printMsg:
-#            print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
-#            
-#    @make_method('/muse/config', 's')
-#    def config_callback(self, path, args):
-#        if not self.configFound:
-#            self.config = args
-#            self.configFound = True
-#            # TODO: TRANSFORM JSON TO DICTIONARY!!!
-#
-#    #handle unexpected messages
-#    @make_method(None, None)
-#    def fallback(self, path, args, types, src):
-#        if printMsg:
-#            print "Unknown message \
-#            \n\t Source: '%s' \
-#            \n\t Address: '%s' \
-#            \n\t Types: '%s ' \
-#            \n\t Payload: '%s'" \
-#            % (src.url, path, types, args)   
+            if self.printMsg:
+                print '%s %f %f %f %f' % (addr, l_ear, l_forehead, r_forehead, r_ear)
+                
+    def format_acc_handler(self, addr, tags, packet, source):
+        if addr=='/muse/acc':
+            acc1, acc2, acc3 = packet
+            if self.printMsg:
+                print '%s %f %f %f' % (addr, acc1, acc2, acc3)
+                
+    def format_config_handler(self, addr, tags, packet, source):
+        if addr=='/muse/config' and not self.configFound:
+            self.config = packet
+            self.configFound = True
+            print qwfuibobs
+                
+    def format_other_handler(self, addr, tags, packet, source):
+        pass
+
+    def config_callback(self, path, args):
+        if not self.configFound:
+            self.config = args
+            self.configFound = True
       
-    def getparams(self):
+    def getMuseConfig(self):
         if self.configFound:
             out = self.config
+            # TODO: Parse JSON!
         else:
             out = None
             
@@ -77,6 +76,7 @@ if __name__ == "__main__":
     
     try:
         server = MuseServer(printMsg=True)
+        server.startServer()
         
     except 'allo':
         
@@ -84,12 +84,3 @@ if __name__ == "__main__":
         
         print str(err)
         sys.exit()
-    
-#    try:
-#        server = MuseServer(printMsg=True)
-#        
-#    
-#    server.start()
-#    
-#    while 1:
-#        time.sleep(1)
