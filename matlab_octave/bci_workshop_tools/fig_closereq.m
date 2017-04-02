42738f8b17391fe438de4fc23be277dc3eaf823f
Ray Cassani <raymundo.cassani@gmail.com>
2017-04-02 13:18:59 -0400

    update in fig_closereq.m. Now it does not delete the current figure

--- matlab_octave/bci_workshop_tools/fig_closereq.m
+++ matlab_octave/bci_workshop_tools/fig_closereq.m
@@ -2,7 +2,5 @@ function fig_closereq(src,callbackdata)
 global stop_loop
 % Figure close request function
 % Changes the value of the global variable STOP_LOOP to True
-% to display a question dialog box 
 stop_loop = true;
-delete(gcf);
 end
